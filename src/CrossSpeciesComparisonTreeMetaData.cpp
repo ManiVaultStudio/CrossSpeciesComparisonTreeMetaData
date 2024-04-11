24457f9b1bbc29f28bbd6ec642070afbb4f5c952
Soumyadeep Basu <44787782+basusoumyadeep@users.noreply.github.com>
2024-04-11 15:38:32 +0200

    serialize data

--- src/CrossSpeciesComparisonTreeMetaData.cpp
+++ src/CrossSpeciesComparisonTreeMetaData.cpp
@@ -27,45 +27,22 @@ CrossSpeciesComparisonTreeMetaData::~CrossSpeciesComparisonTreeMetaData(void)
 
 void CrossSpeciesComparisonTreeMetaData::fromVariantMap(const QVariantMap& variantMap)
 {
-    /*variantMapMustContain(variantMap, "CrossSpeciesComparisonTreeMetaInfo");
-    variantMapMustContain(variantMap, "LeafInfo");
-    variantMapMustContain(variantMap, "PropertyInfo");
+    RawData::fromVariantMap(variantMap);
 
-    const auto data = variantMap["CrossSpeciesComparisonTreeMetaInfo"].toMap();
-    const auto numberOfPoints = static_cast<size_t>(variantMap["LeafInfo"].toInt());
-    const auto numberOfDimensions = variantMap["PropertyInfo"].toUInt();
-
-
-    const auto elementTypeIndex = static_cast<CrossSpeciesComparisonTreeMetaData::ElementTypeSpecifier>(data["TypeIndex"].toInt());
-    const auto rawData = data["Raw"].toMap();
-
-
-    setElementTypeSpecifier(elementTypeIndex);
-    resizeVector(numberOfElements);
-    populateDataBufferFromVariantMap(rawData, (char*)getDataVoidPtr());
-    _numDimensions = numberOfDimensions;
-    */
+    if (variantMap.contains("Data")) {
+        QJsonDocument doc = QJsonDocument::fromJson(variantMap["Data"].toByteArray());
+        setTreeMetaDataRaw(doc.object());
+    }
 }
 
 QVariantMap CrossSpeciesComparisonTreeMetaData::toVariantMap() const
 {
-    QVariantMap rawData;
-    /*
-    const auto typeSpecifier = getElementTypeSpecifier();
-    const auto typeSpecifierName = getElementTypeNames()[static_cast<std::int32_t>(typeSpecifier)];
-    const auto typeIndex = static_cast<std::int32_t>(typeSpecifier);
-    const auto numberOfElements = getNumberOfElements();
+    QVariantMap variantMap = RawData::toVariantMap();
 
-    QVariantMap rawData = rawDataToVariantMap((const char*)getDataConstVoidPtr(), getRawDataSize(), true);
+    QJsonDocument doc(_data);
 
-    return {
-        { "TypeIndex", QVariant::fromValue(typeIndex) },
-        { "TypeName", QVariant(typeSpecifierName) },
-        { "Raw", QVariant::fromValue(rawData) },
-        { "NumberOfElements", QVariant::fromValue(numberOfElements) }
-    };
-    */
-    return rawData;
+    variantMap["Data"] = doc.toJson();
+    return variantMap;
 }
 
 void CrossSpeciesComparisonTreeMetaData::init()
@@ -247,146 +224,17 @@ QString& CrossSpeciesComparisonTreeMeta::getTreeMetaPropertyNames()
 
 void CrossSpeciesComparisonTreeMeta::fromVariantMap(const QVariantMap& variantMap)
 {
-    /*
     DatasetImpl::fromVariantMap(variantMap);
 
-    variantMapMustContain(variantMap, "DimensionNames");
-    variantMapMustContain(variantMap, "Selection");
-
-    // For backwards compatibility, check PluginVersion
-    if (variantMap["PluginVersion"] == "No Version" && !variantMap["Full"].toBool())
-    {
-        makeSubsetOf(getParent()->getFullDataset<mv::DatasetImpl>());
-
-        qWarning() << "[ManiVault deprecation warning]: This project was saved with an older ManiVault version (<1.0). "
-            "Please save the project again to ensure compatibility with newer ManiVault versions. "
-            "Future releases may not be able to load this projects otherwise. ";
-    }
-
-    // Load raw point data
-    if (isFull())
-        getRawData<PointData>()->fromVariantMap(variantMap);
-    else
-    {
-        variantMapMustContain(variantMap, "Indices");
-
-        const auto& indicesMap = variantMap["Indices"].toMap();
-
-        indices.resize(indicesMap["Count"].toInt());
-
-        populateDataBufferFromVariantMap(indicesMap["Raw"].toMap(), (char*)indices.data());
-    }
-
-    // Load dimension names
-    QStringList dimensionNameList;
-    std::vector<QString> dimensionNames;
-
-    // Fetch dimension names from map
-    const auto fetchDimensionNames = [&variantMap]() -> QStringList {
-        QStringList dimensionNames;
-
-        // Dimension names in byte array format
-        QByteArray dimensionsByteArray;
-
-        // Copy the dimension names raw data into the byte array
-        dimensionsByteArray.resize(variantMap["DimensionNames"].toMap()["Size"].value<std::uint64_t>());
-        populateDataBufferFromVariantMap(variantMap["DimensionNames"].toMap(), (char*)dimensionsByteArray.data());
-
-        // Open input data stream
-        QDataStream dimensionsDataStream(&dimensionsByteArray, QIODevice::ReadOnly);
-
-        // Stream the data to the dimension names
-        dimensionsDataStream >> dimensionNames;
-
-        return dimensionNames;
-        };
-
-    if (variantMap["NumberOfDimensions"].toInt() > 1000)
-        dimensionNameList = fetchDimensionNames();
-    else
-        dimensionNameList = variantMap["DimensionNames"].toStringList();
-
-    if (dimensionNameList.size() == getNumDimensions())
-    {
-        for (const auto& dimensionName : dimensionNameList)
-            dimensionNames.push_back(dimensionName);
-    }
-    else
-    {
-        for (std::uint32_t dimensionIndex = 0; dimensionIndex < getNumDimensions(); dimensionIndex++)
-            dimensionNames.emplace_back(QString("Dim %1").arg(QString::number(dimensionIndex)));
-    }
-
-    setDimensionNames(dimensionNames);
-
-    if (variantMap.contains("Dimensions")) {
-        _dimensionsPickerAction->fromParentVariantMap(variantMap);
-    }
-
-    events().notifyDatasetDataChanged(this);
-
-    // Handle saved selection
-    if (isFull()) {
-        const auto& selectionMap = variantMap["Selection"].toMap();
-
-        const auto count = selectionMap["Count"].toInt();
-
-        if (count > 0) {
-            auto selectionSet = getSelection<Points>();
-
-            selectionSet->indices.resize(count);
-
-            populateDataBufferFromVariantMap(selectionMap["Raw"].toMap(), (char*)selectionSet->indices.data());
-
-            events().notifyDatasetDataSelectionChanged(this);
-        }
-    }
-    */
+    getRawData<CrossSpeciesComparisonTreeMetaData>()->fromParentVariantMap(variantMap);
+    events().notifyDatasetDataSelectionChanged(this);
 }
 
 QVariantMap CrossSpeciesComparisonTreeMeta::toVariantMap() const
 {
-    QVariantMap variantMap;
-    /*
     auto variantMap = DatasetImpl::toVariantMap();
 
-    QStringList dimensionNames;
-    QByteArray dimensionsByteArray;
-    QDataStream dimensionsDataStream(&dimensionsByteArray, QIODevice::WriteOnly);
+    getRawData<CrossSpeciesComparisonTreeMetaData>()->insertIntoVariantMap(variantMap);
 
-    if (getDimensionNames().size() == getNumDimensions()) {
-        for (const auto& dimensionName : getDimensionNames())
-            dimensionNames << dimensionName;
-    }
-    else {
-        for (std::uint32_t dimensionIndex = 0; dimensionIndex < getNumDimensions(); dimensionIndex++)
-            dimensionNames << QString("Dim %1").arg(QString::number(dimensionIndex));
-    }
-
-    if (dimensionNames.size() > 1000)
-        dimensionsDataStream << dimensionNames;
-
-    QVariantMap indices;
-
-    indices["Count"] = QVariant::fromValue(this->indices.size());
-    indices["Raw"] = rawDataToVariantMap((char*)this->indices.data(), this->indices.size() * sizeof(std::uint32_t), true);
-
-    QVariantMap selection;
-
-    if (isFull()) {
-        auto selectionSet = getSelection<Points>();
-
-        selection["Count"] = QVariant::fromValue(selectionSet->indices.size());
-        selection["Raw"] = rawDataToVariantMap((char*)selectionSet->indices.data(), selectionSet->indices.size() * sizeof(std::uint32_t), true);
-    }
-
-    variantMap["Data"] = isFull() ? getRawData<PointData>()->toVariantMap() : QVariantMap();
-    variantMap["NumberOfPoints"] = getNumPoints();
-    variantMap["Indices"] = indices;
-    variantMap["Selection"] = selection;
-    variantMap["DimensionNames"] = (dimensionNames.size() > 1000) ? rawDataToVariantMap((char*)dimensionsByteArray.data(), dimensionsByteArray.size(), true) : QVariant::fromValue(dimensionNames);
-    variantMap["NumberOfDimensions"] = getNumDimensions();
-    variantMap["Dimensions"] = _dimensionsPickerAction->toVariantMap();
-    */
     return variantMap;
 }
