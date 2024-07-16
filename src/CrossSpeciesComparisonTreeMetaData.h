98f649fa5fd767dd638ee1804ef2b7ff99439de6
Soumyadeep Basu <44787782+basusoumyadeep@users.noreply.github.com>
2024-07-16 18:52:06 +0200

    fix for linux[skip ci]

--- src/CrossSpeciesComparisonTreeMetaData.h
+++ src/CrossSpeciesComparisonTreeMetaData.h
@@ -1,5 +1,5 @@
 #pragma once
-#include "CrossSpeciesComparisonTreeMetaData_export.h"
+#include "crossspeciescomparisontreemetadata_export.h"
 #include <RawData.h>
 #include <Set.h>
 #include <QJsonObject>
